e06933386e51cc21b991ddc9d0cc9a75d3b6449a
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-02-26 12:37:14 -0700

    Initial commit for slots

---
+++ slots/slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by Jonas Knudsen on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
