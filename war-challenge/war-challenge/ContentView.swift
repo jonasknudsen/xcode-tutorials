e28da7572de880a916cd506bd0ac704aec566888
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-02-26 01:43:51 -0700

    Created initial layout

--- war-challenge/war-challenge/ContentView.swift
+++ war-challenge/war-challenge/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            // title pic
+            Image("logo")
+            Spacer()
+            
+            // two cards
+            HStack {
+                Image("card3")
+                    .padding(25)
+                Image("card4")
+                    .padding(25)
+            }
+            Spacer()
+            
+            // deal button
+            Image("dealbutton")
+            Spacer()
+            
+            // scores
+            HStack {
+                Spacer()
+                VStack {
+                    Text("Player")
+                        .padding(3)
+                    Text("0")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+                .foregroundColor(.white)
+                
+                Spacer()
+                
+                VStack {
+                    Text("CPU")
+                        .padding(3)
+                    Text("0")
+                        .font(.title)
+                }
+                .foregroundColor(.white)
+                
+                Spacer()
+            }
+            Spacer()
+        }
+        .background(Image("background")
+                        .ignoresSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, edges: /*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .aspectRatio(contentMode: .fit))
     }
 }
 
