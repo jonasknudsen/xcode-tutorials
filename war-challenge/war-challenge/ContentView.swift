58e81459c94ea3e7ddb2ea3d11ea6a6d3386816e
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-02-26 01:54:49 -0700

    Changed SwiftUI to match the example

--- war-challenge/war-challenge/ContentView.swift
+++ war-challenge/war-challenge/ContentView.swift
@@ -9,53 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            // title pic
-            Image("logo")
-            Spacer()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
             
-            // two cards
-            HStack {
-                Image("card3")
-                    .padding(25)
-                Image("card4")
-                    .padding(25)
-            }
-            Spacer()
-            
-            // deal button
-            Image("dealbutton")
-            Spacer()
-            
-            // scores
-            HStack {
+            VStack {
                 Spacer()
-                VStack {
-                    Text("Player")
-                        .padding(3)
-                    Text("0")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                // title pic
+                Image("logo")
+                
+                Spacer()
+                
+                // two cards
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
                 }
-                .foregroundColor(.white)
                 
                 Spacer()
                 
-                VStack {
-                    Text("CPU")
-                        .padding(3)
-                    Text("0")
-                        .font(.title)
+                // deal button
+                Image("dealbutton")
+                
+                Spacer()
+                
+                // scores
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    .foregroundColor(.white)
+                    
+                    Spacer()
                 }
-                .foregroundColor(.white)
                 
                 Spacer()
             }
-            Spacer()
         }
-        .background(Image("background")
-                        .ignoresSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, edges: /*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                        .aspectRatio(contentMode: .fit))
     }
 }
 
