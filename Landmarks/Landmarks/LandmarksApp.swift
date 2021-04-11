9fe001e2fbefdeb43cc1637e84255c3d88e0b0ca
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-04-11 12:38:39 -0600

    Started SwiftUI Landmarks tutorial app

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jonas Knudsen on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
