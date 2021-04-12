34bdca3959946989670c8f7bc4f09015c4f8d7ec
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-04-12 01:15:41 -0600

    Added CoreDataDemo

---
+++ CoreDataDemo/CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Jonas Knudsen on 4/12/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
